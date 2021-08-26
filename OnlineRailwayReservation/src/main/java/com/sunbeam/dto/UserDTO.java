2bacec83e3cd01298378037b4fbaf1a9ce53cd82
arohablue <amit42308@gmail.com>
2021-08-26 17:33:05 +0530

    updated user DTO

--- OnlineRailwayReservation/src/main/java/com/sunbeam/dto/UserDTO.java
+++ OnlineRailwayReservation/src/main/java/com/sunbeam/dto/UserDTO.java
@@ -9,7 +9,7 @@ import com.sunbeam.entity.User;
 import org.springframework.beans.BeanUtils;
 
 public class UserDTO {
-	private int id;
+	private int userId;
 	private String email;
 	private String password;
 	private int age;
@@ -18,13 +18,15 @@ public class UserDTO {
 	private String state;
 	private String city;
 	private String role;
+
 	public UserDTO() {
-		
+
 	}
-	public UserDTO(int id, String email, String password, int age, String gender, String mobile, String state,
+
+	public UserDTO(int userId, String email, String password, int age, String gender, String mobile, String state,
 			String city, String role) {
 		super();
-		this.id = id;
+		this.userId = userId;
 		this.email = email;
 		this.password = password;
 		this.age = age;
@@ -35,135 +37,96 @@ public class UserDTO {
 		this.role = role;
 	}
 
-	public int getId() {
-		return id;
+	public int getUserId() {
+		return userId;
 	}
 
-
-
-	public void setId(int id) {
-		this.id = id;
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 
 	public int getAge() {
 		return age;
 	}
 
-
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-
 
 	public String getMobile() {
 		return mobile;
 	}
 
-
-
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
-
-
 
 	public String getState() {
 		return state;
 	}
 
-
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
-
 
 	public String getCity() {
 		return city;
 	}
 
-
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
-
 
 	public String getRole() {
 		return role;
 	}
 
-
-
 	public void setRole(String role) {
 		this.role = role;
 	}
 
-	
-
 	@Override
 	public String toString() {
-		return "UserDTO [id=" + id + ", email=" + email + ", password=" + password + ", age=" + age + ", gender="
+		return "UserDTO [id=" + userId + ", email=" + email + ", password=" + password + ", age=" + age + ", gender="
 				+ gender + ", mobile=" + mobile + ", state=" + state + ", city=" + city + ", role=" + role + "]";
 	}
-
-
 
 	public static UserDTO fromEntity(User user) {
 		UserDTO dto = new UserDTO();
 		BeanUtils.copyProperties(user, dto);
-		dto.setId(user.getId());
+		dto.setUserId(user.getId());
 		return dto;
 	}
-	
+
 	public static User toEntity(UserDTO dto) {
 		User user = new User();
 		BeanUtils.copyProperties(dto, user);
-		user.setId(dto.getId());
+		user.setId(dto.getUserId());
 		return user;
 	}
-	
-	
+
 }
