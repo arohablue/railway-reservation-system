08de3c4d3899cae2766cfae3908c363a665952dc
rhnmahajan83 <rhnmahajan83@gmail.com>
2021-08-30 20:24:36 +0530

    check pnr integration

--- React/portal/src/pages/TicketDetails.js
+++ React/portal/src/pages/TicketDetails.js
@@ -16,23 +16,23 @@ export default function TicketDetails() {
     <div className="form-control">
       <h1 className="page-title">Ticket Details</h1>
       <div>
-        <span>Ticket Number: </span>
+        <span className="grey-text">Ticket Number: </span>
         <span>{ticketDetails.train.trainNumber}</span>
       </div>
       <div>
-        <span>PNR number: </span>
+        <span className="grey-text">PNR number: </span>
         <span>{ticketDetails.pnr}</span>
       </div>
       <div>
-        <span>PNR status: </span>
+        <span className="grey-text">PNR status: </span>
         <span>{ticketDetails.status}</span>
       </div>
       <div>
-        <span>Reservation Date: </span>
+        <span className="grey-text">Reservation Date: </span>
         <span>{ticketDetails.reservationDate}</span>
       </div>
       <div>
-        <span>Email: </span>
+        <span className="grey-text">Email: </span>
         <span>{ticketDetails.user.email}</span>
       </div>
     </div>
