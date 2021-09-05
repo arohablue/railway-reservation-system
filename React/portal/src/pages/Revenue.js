2284eb2b153aed21a82df6c847a2ecf4dce9ed6e
arohablue <amit42308@gmail.com>
2021-09-06 00:05:58 +0530

    added revenue tab

--- React/portal/src/pages/Revenue.js
+++ React/portal/src/pages/Revenue.js
@@ -2,65 +2,50 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: [
+    "JAN",
+    "FEB",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUG",
+    "SEPT",
+    "OCT",
+    "NOV",
+    "DEC",
+  ],
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
+      label: "AC Revenue",
+      data: [1200, 1900, 300, 500, 200, 300, 1200, 1900, 300, 500, 200, 300],
+      backgroundColor: "rgb(252, 3, 3)",
+    },
+    {
+      label: "Gen Revenue",
+      data: [200, 300, 2000, 500, 100, 400, 200, 300, 2000, 500, 100, 400],
+      backgroundColor: "rgb(54, 162, 235)",
     },
   ],
 };
 
 const options = {
-  indexAxis: "y",
-  // Elements options apply to all of the options unless overridden in a dataset
-  // In this case, we are setting the border of each horizontal bar to be 2px wide
-  elements: {
-    bar: {
-      borderWidth: 2,
-    },
-  },
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "right",
-    },
-    title: {
-      display: true,
-      text: "Chart.js Horizontal Bar Chart",
-    },
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
   },
 };
 
 const Revenue = () => (
   <>
     <div className="header">
-      <h1 className="title">Horizontal Bar Chart</h1>
-      <div className="links">
-        <a
-          className="btn btn-gh"
-          href="https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/HorizontalBar.js"
-        >
-          Github Source
-        </a>
-      </div>
+      <h1 className="title">Revenue</h1>
     </div>
     <Bar data={data} options={options} />
   </>
